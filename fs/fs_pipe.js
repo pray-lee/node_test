21d1194ea7ce40924b76771e764c1628449cf7c1
pray-lee <praycis@gmail.com>
2018-10-10 10:06:16 +0800

    update stream

--- fs/fs_pipe.js
+++ fs/fs_pipe.js
@@ -2,5 +2,7 @@ const fs = require('fs')
 
 const readStream = fs.createReadStream(`${__dirname}/file.txt`)
 const writeStream = fs.createWriteStream(`${__dirname}/filePipe.txt`)
+
+
 //管道操作
 readStream.pipe(writeStream)
